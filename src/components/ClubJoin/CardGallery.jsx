84110bae8d579b9a2a266a1b5d5a95e1c5b279c8
Giyas <giyas231@gmail.com>
2022-12-30 02:01:56 +0700

    tampilang club buat yang belum join

--- src/components/ClubJoin/CardGallery.jsx
+++ src/components/ClubJoin/CardGallery.jsx
@@ -4,15 +4,9 @@ import { Card, CardHeader, CardBody, CardFooter, SimpleGrid, Text, Image, Headin
 const CardGallery = () => {
   return (
     <div>
-        <Card bg='white' variant={'filled'} shadow='xl'>
+        <Card variant={'filled'} >
             <CardBody>
             <SimpleGrid spacing={8} w={'100%'} h={"30%"} columns={{sm: 2, md: 4}}> 
-                    <Image
-                    src='https://e00-marca.uecdn.es/assets/multimedia/imagenes/2022/11/16/16685771908226.jpg'
-                    width={'300px'}
-                    height={'300px'}
-                    objectFit='cover'
-                    />
                     <Image
                     src='https://e00-marca.uecdn.es/assets/multimedia/imagenes/2022/11/16/16685771908226.jpg'
                     width={'300px'}
