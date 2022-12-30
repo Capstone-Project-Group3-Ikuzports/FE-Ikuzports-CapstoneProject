5099aed0592f4fe8f008230d3684ce524065734d
Giyas <giyas231@gmail.com>
2022-12-31 02:50:59 +0700

    get card product param butuh endpoin param

--- src/components/Store/CardProduct.jsx
+++ src/components/Store/CardProduct.jsx
@@ -2,24 +2,24 @@ import React from 'react'
 import { Card, Flex, CardBody, CardFooter, Text, Image, Stack, Divider, Heading, Spacer } from '@chakra-ui/react'
 import {BsFillTrashFill} from 'react-icons/bs'
 
-const CardProduct = () => {
+const CardProduct = ({image,nama,harga}) => {
   return (
     <div>
         <Card maxW='sm' bg='white' _hover={{cursor: 'pointer'}}>
         <CardBody>
             <Image
-            src='https://images.hothardware.com/contentimages/article/3079/content/small_samsung-980-angle.jpg'
+            src={image}
             alt='Green double couch with wooden legs'
             borderRadius='lg'
             mx='auto'
             />
             <Stack mt='3' spacing='3'>
             <Flex mb={10}>
-                <Heading size='md'>SSD SAMSUNG 980 EVO 1TB</Heading>
+                <Heading size='md'>{nama}</Heading>
                 <Spacer></Spacer>
             </Flex>
             <Text color='blue.600' as='b' fontSize='2xl'>
-                Rp 420.000
+               {harga}
             </Text>
             </Stack>
         </CardBody>
--- src/pages/Product/Store.jsx
+++ src/pages/Product/Store.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
+import React,{useState,useEffect} from 'react'
+
 import {AiOutlineArrowLeft, AiOutlineSearch} from 'react-icons/ai'
 import { Box, Text, Flex, Divider, Button, SimpleGrid, Card, ButtonGroup, Select, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import CardProduct from '../../components/Store/CardProduct'
 import Layout from '../../components/Layout';
+import axios from 'axios';
 
 const Store = () => {
+const [product,setProduct] = useState([]);
+
+const getProduct = () =>{
+  axios
+  .get('https://rubahmerah.site/products',
+  {headers:{authorization : `bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE2NzI1MDM3OTQsIm5hbWUiOiJNaWNoYWVsIEpvcmRhbiIsInJvbGUiOiJVc2VyIiwidXNlcklkIjoyfQ.N86NGjluPa99D1OC2EzgtZVuDdaA-BAj54ywsIPIYo4`}})
+  .then(res=>{
+    setProduct(res.data.data)
+    console.log("ini adahlah",res.data.data)
+  })
+}
+
+useEffect(()=>
+  getProduct(),[]
+)
+  
+
   return (
 <Layout>
       <Box p='8' px={'10%'} w={'100vw'} h={'100%'} overflowX='hidden'>
@@ -13,46 +31,45 @@ const Store = () => {
         <AiOutlineArrowLeft size={41}/> 
         <Text fontSize={'2xl'} as='b' ml='10px'>Back</Text>
         </Flex>
-        <Text fontSize={'6xl'}>Store</Text>
+        <Text fontSize={'5xl'} as='u' textColor={'brand.300'}>Store</Text>
         <Divider w="17%" orientation='horizontal' />
         <Flex>
         <Box  ml={'57%'}>
           <Flex mb='30px'>
-           <Select w={'200px'} bg='white' mr='30px' variant='filled' boxShadow={'xl'} placeholder='Filled'>
+           <Select w={'200px'} bg='white' mr='30px' variant='filled' boxShadow={'xl'} placeholder='Category'>
+                  <option value= '1' >Sepatu</option>
+                  <option value= '2'>Jersey</option>
+                  <option value= '3'>Bola</option>
+                  <option value= '4'>Raket</option>
+                  <option value= '5'>Celana</option>
+                  <option value= '6'>Equipment</option>
+                  <option value= '7'>Aksesoris</option>
+           </Select>
+           <Select w={'200px'} bg='white' mr='30px' variant='filled' boxShadow={'xl'} placeholder='City'>
            <option value='option1'>Option 1</option>
            <option value='option2'>Option 2</option>
            <option value='option3'>Option 3</option>
            </Select>
-           <Select w={'200px'} bg='white' mr='30px' variant='filled' boxShadow={'xl'} placeholder='Filled'>
-           <option value='option1'>Option 1</option>
-           <option value='option2'>Option 2</option>
-           <option value='option3'>Option 3</option>
-           </Select>
-           <InputGroup w={'200px'} boxShadow={'xl'} varian='filled'>
+           <InputGroup w={'250px'} boxShadow={'xl'} varian='filled'>
               <InputLeftElement
                 pointerEvents='none'
                 children={<AiOutlineSearch color='gray.300' />}
               />
-              <Input type='tel' bg='white' placeholder='Phone number' />
+              <Input type='tel' bg='white' placeholder='Search Product' />
             </InputGroup>
           </Flex>
         </Box>
         </Flex>
         <SimpleGrid columns={{sm:2, md:4}} gap={8}>
-        <CardProduct/>
-        <CardProduct/>
-        <CardProduct/>
-        <CardProduct/>
-        <CardProduct/>
-        <CardProduct/>
-        <CardProduct/>
-        <CardProduct/>
-        <CardProduct/>
-        <CardProduct/>
-        <CardProduct/>
-        <CardProduct/>
-        <CardProduct/>
-        <CardProduct/>
+        
+        {product.map((item)=>(
+              <CardProduct 
+              image=''
+              nama={item.name}
+              harga={item.price}/>
+        ))}
+    
+       
         </SimpleGrid>
       </Box>
       </Layout>
